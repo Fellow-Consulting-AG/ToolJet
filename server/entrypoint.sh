c2a338da44844a354d9018cbc7290a6d051f61e9
FellowFellow <benedikt.liebs@fellow-consulting.de>
2022-06-30 13:57:16 +0200

    update

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -d "./server/dist" ]
-then
-    # npm run db:setup
-    npm run db:migrate
-    # npm run db:setup:prod
-else
-    npm run db:setup
-fi
+npm run db:migrate
+npm run start:prod
+
 
 exec "$@"
