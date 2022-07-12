8252e6e9b4d2dbb303a03510ebf3142624527dfd
FellowFellow <benedikt.liebs@fellow-consulting.de>
2022-07-12 14:10:06 +0200

    push to dev

--- server/ee/services/oauth/polydocs_oauth.service.ts
+++ server/ee/services/oauth/polydocs_oauth.service.ts
@@ -6,6 +6,7 @@ import UserResponse from './models/user_response';
 @Injectable()
 export class PolydocsOAuthService {
   private prefix = '';
+  private suffix = '';
   constructor(private readonly configService: ConfigService) {
     // only use prefix to determine the auth service url
     if (this.configService.get<string>('PREFIX') === 'prod' || this.configService.get<string>('PREFIX') === 'sandbox') {
@@ -13,47 +14,69 @@ export class PolydocsOAuthService {
     } else {
       this.prefix = this.configService.get<string>('PREFIX') + '.';
     }
+
+    if (this.configService.get<string>('TOOLJET_HOST').slice(-1) !== '/') {
+      this.suffix = '/';
+    }
   }
-  private readonly authUrl = 'https://' + this.prefix + 'auth.cloudintegration.eu/oauth2/token';
+
+  private readonly authUrl = 'https://' + this.prefix + 'auth.cloudintegration.eu/oauth2/authorize';
+  private readonly tokenUrl = 'https://' + this.prefix + 'auth.cloudintegration.eu/oauth2/token';
   private readonly getUserUrl = 'https://' + this.prefix + 'auth.cloudintegration.eu/api/me';
-  private readonly getUserEmailUrl = 'https://' + this.prefix + 'auth.cloudintegration.eu/api/me';
+  private readonly serverUrl = this.configService.get<string>('TOOLJET_HOST') + this.suffix;
+
 
   // AuthResponse will need to be changed!
   async #getUserDetails({ access_token }: AuthResponse): Promise<UserResponse> {
     const response: any = await got(this.getUserUrl, {
       method: 'get',
-      headers: { Accept: 'application/json', Authorization: `token ${access_token}` },
+      headers: { Accept: 'application/x-www-form-urlencoded', Authorization: `Bearer ${access_token}` },
     }).json();
 
-    const { name } = response;
-    let { email } = response;
-    const words = name?.split(' ');
-    const firstName = words?.[0] || '';
-    const lastName = words?.length > 1 ? words[words.length - 1] : '';
+    const email = response.data.username;
+    const firstName = response.data.first_name;
+    const lastName = response.data.last_name;
 
-    if (!email) {
-      // email visibility not set to public
-      email = await this.#getEmailId(access_token);
-    }
-
-    return { userSSOId: access_token, firstName, lastName, email, sso: 'git' };
-  }
-
-  async #getEmailId(access_token: string) {
-    const response: any = await got(this.getUserEmailUrl, {
-      method: 'get',
-      headers: { Accept: 'application/json', Authorization: `X-API-KEY ${access_token}` },
-    }).json();
-
-    return response?.find((emails) => emails.primary)?.email;
+    return { userSSOId: access_token, firstName, lastName, email, sso: 'polydocs' };
   }
 
   async signIn(code: string, configs: any): Promise<any> {
-    const response: any = await got(this.authUrl, {
-      method: 'post',
+    /* auth code -> new endpoint
+
+    I want to open up the login page and login the user -> by chance pass the email + password to the login automatically.
+
+    after login i need the token 
+    scope -> profile
+
+
+
+
+    signIn -> login on auth -> token -> getUserDetails -> setup user
+
+    1. get call grant_type: authorization_code, client_id, redirect_uri -> insight uri
+    2. get code -> send request to token
+    */
+    // const formData = new FormData();
+    const token = 'Basic ' + new Buffer(configs.clientId + ':' + configs.clientSecret, 'base64').toString();
+
+    const url =
+      this.authUrl +
+      '?client_id=' +
+      configs.clientId +
+      '&scope=profile' +
+      '&redirect_uri=' +
+      this.serverUrl +
+      'login/oauth/authorize' +
+      '&response_type=code';
+
+    console.log('token for auth: ' + token);
+    console.log('url for auth: ' + url);
+
+    const response: any = await got(url, {
+      method: 'get',
       headers: {
         Accept: 'application/json',
-        Authorization: 'Bearer ' + btoa(configs.clientId + ':' + configs.clientSecret),
+        Authorization: token,
       },
     }).json();
 
